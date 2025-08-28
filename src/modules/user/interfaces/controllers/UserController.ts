ba585be7d9a9678dbee322e42aeca606b70c1341
Iank-code <kian99564@gmail.com>
2025-08-29 00:31:07 +0300

    setup

--- src/modules/user/interfaces/controllers/UserController.ts
+++ src/modules/user/interfaces/controllers/UserController.ts
@@ -1,5 +1,12 @@
+import { Request, Response } from "express";
 export class UserController {
-  async register(req: Request, res: Response) { … }
-  async login(req: Request, res: Response) { … }
-  async getById(req: Request, res: Response) { … }
+  async register(req: Request, res: Response) {
+    // TODO: implement registration logic
+  }
+  async login(req: Request, res: Response) {
+    // TODO: implement login logic
+  }
+  async getById(req: Request, res: Response) {
+    // TODO: implement getById logic
+  }
 }
